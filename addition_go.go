77636f41b5a27c42176253b8db6f67740d490d78
‘ShivaGuntuku’ <guntuku.shiva@gmail.com>
2021-08-11 10:51:25 +0530

    inital push GO Learning

---
+++ addition_go.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+// var a, b int
+
+func main() {
+	a := 22
+	b := 45
+	d := a + b
+	fmt.Println("Hello World", d)
+}
---
+++ hexadecimal_ex.go
@@ -0,0 +1,9 @@
+package main
+import  "fmt"
+
+func main() {
+	// n := 2548
+	y := "browsing under an umbrella"
+	// fmt.Printf("%x", n)
+	fmt.Printf("%b", y)
+}
